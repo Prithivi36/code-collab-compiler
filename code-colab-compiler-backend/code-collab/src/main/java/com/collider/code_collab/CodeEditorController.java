32b408330c3e489e9c4c6d5a083cb771f8d8a88d
Prithivi36 <prithivipappian@gmail.com>
2025-08-02 09:39:21 +0530

    in memory code storage for new users

--- code-colab-compiler-backend/code-collab/src/main/java/com/collider/code_collab/CodeEditorController.java
+++ code-colab-compiler-backend/code-collab/src/main/java/com/collider/code_collab/CodeEditorController.java
@@ -4,16 +4,32 @@ import lombok.AllArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @AllArgsConstructor
 public class CodeEditorController {
 
+    SimpMessagingTemplate messagingTemplate;
+    private final Map<String,String > map = new HashMap<>();
+
     @MessageMapping("/room/{roomId}/edit")
     @SendTo("/topic/room/{roomId}")
     public CodeEditor codeSocket(@DestinationVariable String roomId, CodeEditor codeEditor) {
+        map.put(roomId, codeEditor.getContent());
         return codeEditor;
+    }
+
+    @MessageMapping("room/{roomId}/sync")
+    public void sync(@DestinationVariable String roomId) {
+        String current = map.get(roomId);
+        CodeEditor latest = new CodeEditor();
+        latest.setContent(current);
+        messagingTemplate.convertAndSend("/topic/room/" + roomId, latest);
     }
 }
