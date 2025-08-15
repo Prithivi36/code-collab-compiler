38a430dfad99fcff154ca1e2bca7f222188358c7
Prithivi36 <prithivipappian@gmail.com>
2025-08-15 17:36:53 +0530

    feedback button

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   
     return(
       <div className="bg-light">
-        
+        <p style={{position:'fixed',bottom:0,right:0,zIndex:'10000',opacity:0.5}} className='bg-secondary mb-0 d-none d-md-block px-2  p-0 text-light'>feedback</p>
         <Body />
       </div>
     )
