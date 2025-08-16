40789fefc75f611e299491fb2a18f03ed863fc63
Prithivi36 <prithivipappian@gmail.com>
2025-08-16 10:02:40 +0530

    FeedBack changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Body from './components/Body'
+import FeedBackModal from './components/FeedBackModal';
 
 function App() {
-  
+    const [dark,setDark] =React.useState(true);
     return(
-      <div className="bg-light">
-        <p style={{position:'fixed',bottom:0,right:0,zIndex:'10000',opacity:0.5}} className='bg-secondary mb-0 d-none d-md-block px-2  p-0 text-light'>feedback</p>
-        <Body />
+      <div className={` ${dark?'bg-prime-dark':'bg-white'} hide-scroll overflow-x-auto`}>
+        <FeedBackModal />
+        <div data-bs-toggle="modal"
+          data-bs-target="#feedback" style={{cursor:'pointer' ,position:'fixed',bottom:0,right:0,zIndex:'10000',opacity:0.3}} className='bg- mb-0 d-none d-md-block px-2 m-0  p-0 text-white'>
+          report <i className="bi ms-1 bi-exclamation-circle"></i></div>
+        <Body dark={dark} setDark={setDark}/>
       </div>
     )
 }
---
+++ src/components/FeedBackModal.jsx
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import React from 'react'
+
+const FeedBackModal = () => {
+    const [rep,setRep]=React.useState("")
+    function handleSubmit(){
+        axios.post("https://comp.back.6thdegree.app/feedback",{
+            message:rep
+        }).catch(
+            err=>alert("something went wrong")
+        )
+    }
+  return (
+    <div id='feedback' className='modal fade'>
+      <div className="modal-dialog modal-dialog-centered modal-lg modal-dialog-scrollable">
+        <div className="modal-content">
+            <div className="modal-body ">
+                <label className='fw-medium mb-3' htmlFor="feedbacktext">What are the issue you experienced ?</label>
+                <textarea onChange={(e)=>setRep(e.target.value)} placeholder='send bug report' id="feedbacktext" className='form-control' name="" ></textarea>
+                <p onClick={handleSubmit} className='text-end'>
+                    <button data-bs-dismiss='modal'  className="text-primary text-end btn">Submit</button>
+                </p>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FeedBackModal
